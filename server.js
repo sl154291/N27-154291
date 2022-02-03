0708e9446ebed8b70e4ef59650136bce2561fb4f
sl154291 <sl154291@students.berufskolleg-borken.de>
2022-02-03 09:09:38 +0100

    Benutzerdaten entgegennehmen

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
+// Programme verarbeiten oft Objekte der realen Welt.
+// Objekte haben EIgenschaften. 
+// In unserem Bankingprogramm interessieren uns Objekte,
+// wie z.B. Kunde, Konto, Filiale, Bankautomat,...
+// Alle Kunden unserer Bank haben dieselben EIgenschaften, aber 
+// unterschiedliche Eigenschaftswerte 
+
+class Kunde {
+    constructor(){
+      this.IdKunde
+      this.Nachname 
+      this.Vorname 
+      this.Kennwort 
+      this.Kontostand 
+      this.Geburtsdatum 
+      this.Mail
+    }
+}
+
+//Von der Kunden-Klasse wird eine komkrete Instanz 
+// gebildet 
+
+let kunde = new Kunde ()
+
+// Die konkrete Instanz bekommt Eigenschaftswerte 
+// zugewiesen 
+
+kunde.IdKunde = 154291
+kunde.Nachname = "Ronaldo"
+kunde.Vorname = "Christiano"
+kunde.Geburtsdatum = "1.1.2000"
+kunde.Mail = "Ronaldo@web.de"
+kunde.Kennwort = "123"
+
+
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -10,7 +45,8 @@ const server = meineApp.listen(process.env.PORT || 3000, () =>
  {console.log('Server lauscht auf Port %s', server.address().port)
  })
  
- meineApp.get('/',(browserAnfrage, serverAntwort, next) => {serverAntwort.render('index.ejs', {})
+meineApp.get('/',(browserAnfrage, serverAntwort, next) => {
+   serverAntwort.render('index.ejs', {})
 })
  
  // Wenn die login-Seite im Browser aufgerufen wird,...
@@ -20,11 +56,26 @@ const server = meineApp.listen(process.env.PORT || 3000, () =>
       
     // ... dann wird die login.ejs vom Server gerendert an den
     // Browser zurückgegeben:    
-    serverAntwort.render('login.ejs', {}) })
-    
-    // Die meineApp.post('login')wird ausgeführt sobald der Butten auf dem loginformular gedrückt wird.
-    meineApp.post('/login',(browserAnfrage, serverAntwort, next) => {
-    serverAntwort.render('index.ejs', {}) 
+    serverAntwort.render('login.ejs', {})
 }) 
 
 
+
+meineApp.post('/login',(browserAnfrage, serverAntwort, next) => {
+  
+  const idKunde = browserAnfrage.body.IdKunde
+  const kennwort = browserAnfrage.body.Kennwort 
+  
+  console.log("ID des Kunden: " + idKunde)
+  console.log("Kennwort des Kunden: " + kennwort)
+  
+if(idKunde ==kunde.idKunde){
+  serverAntwort.render('index.ejs', {})
+}
+  serverAntwort.render('login.ejs', {})
+})
+
+
+
+  serverAntwort.render('index.ejs', {})
+})
